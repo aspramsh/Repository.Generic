fb5a07f79e14f6e4f008d1feb6e933f615b5208a
Aspram Shadyan <aspramshadyan@gmail.com>
2019-09-09 00:03:56 +0400

    Initial commit.

---
+++ Repository.Generic.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repository.Generic.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
